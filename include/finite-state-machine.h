58a453418b3c0d82f396444e7eae83275b1e1619
Maciej Adamski <madamskip1@gmail.com>
2023-11-04 22:39:28 +0100

    Add traits that check if State has onEntry or onEntry(Event)

--- include/finite-state-machine.h
+++ include/finite-state-machine.h
@@ -20,6 +20,26 @@ namespace FSM
 	using variantTypeFromStatesTuple_t = typename variantTypeFromStatesTuple<StatesTuple>::type;
 
 
+	template <typename StateType, typename EventType, typename = void>
+	struct has_onEntry : std::false_type {};
+
+	template <typename StateType, typename EventType>
+	struct has_onEntry<StateType, EventType, std::void_t<decltype(std::declval<StateType>().onEntry(std::declval<EventType>()))>> : std::true_type {};
+
+	template <typename StateType, typename EventType>
+	inline constexpr bool has_onEntry_v = has_onEntry<StateType, EventType>::value;
+
+	template <typename StateType, typename = void>
+	struct has_onEntryNoEventArg : std::false_type {};
+
+	template <typename StateType>
+	struct has_onEntryNoEventArg<StateType, std::void_t<decltype(std::declval<StateType>().onEntry())>> : std::true_type {};
+
+	template <typename StateType>
+	inline constexpr bool has_onEntryNoEventArg_v = has_onEntryNoEventArg<StateType>::value;
+
+
+
 	template <typename TransitionsTable, typename InitialState = void>
 	class StateMachine
 	{
@@ -33,10 +53,25 @@ namespace FSM
 			InitialState
 		>;
 
-		StateMachine() : statesVariant{ initial_state_type{} } {};
+		StateMachine() : statesVariant{ initial_state_type{} } {};//, statesVariantID{ typeid(initial_state_type)} {};
+
+		template <typename NewState>
+		void forceTransition()
+		{
+			statesVariant.emplace<NewState>();
+		};
+
+		template <typename State>
+		constexpr bool isInState()
+		{
+			return std::holds_alternative<State>(statesVariant);
+		}
+
+
 
 	private:
 		states_variant_type statesVariant;
+		//std::type_info statesVariantID;
 	};
 
 	
--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,28 @@ struct EventA : public FSM::Event {};
 struct EventB : public FSM::Event {};
 
 struct StateA : public FSM::State {};
-struct StateB : public FSM::State {};
-class StateC : public FSM::State {};
-
+struct StateB : public FSM::State {
+    void onEntry() {
+        std::cout << "StateB::onEntry() \n";
+    }
+};
+struct StateC : public FSM::State {
+    void onEntry(const EventA& event)
+    {
+        std::cout << "StateC::onEntry(const FSM::Event& event) \n";
+    }
+    void onEntry(const FSM::Event& event)
+    {
+        std::cout << "StateC::onEntry(const FSM::Event& event) \n";
+    }
+    
+};
+struct StateD : public FSM::State {
+    void onEntry(const EventA& eventA)
+    {
+        std::cout << "StateC::onEntry(const EventA& event) \n";
+    }
+};
 
 int main()
 {
@@ -33,5 +52,39 @@ int main()
     std::cout << std::type_index(typeid(FSM::getStatesFromTransitionsTable_t<Trans>)).name() << '\n';
     std::cout << std::type_index(typeid(FSM::variantTypeFromStatesTuple_t<states_tuple_type>)).name() << '\n';
     auto fsm = FSM::StateMachine<Trans, StateC>{};
+
+    std::cout << "is StateB: " << fsm.isInState<StateB>() << '\n';
+    std::cout << "is StateC: " << fsm.isInState<StateC>() << '\n';
+    fsm.forceTransition<StateB>();
+    std::cout << "is StateB: " << fsm.isInState<StateB>() << '\n';
+    std::cout << "is StateC: " << fsm.isInState<StateC>() << '\n';
+
+    std::cout << "StateA: \n";
+    std::cout << "onEntryNoArgs(): " << FSM::has_onEntryNoEventArg<StateA>::value << std::endl;
+    std::cout << "onEntry(FSM::EventA&): " << FSM::has_onEntry<StateA, EventA>::value << std::endl;
+    std::cout << "onEntryNoArgs(): " << FSM::has_onEntryNoEventArg_v<StateA> << std::endl;
+    std::cout << "onEntry(FSM::EventA&): " << FSM::has_onEntry_v<StateA, EventA> << std::endl;
+
+    std::cout << "StateB: \n";
+    std::cout << "onEntryNoArgs(): " << FSM::has_onEntryNoEventArg<StateB>::value << std::endl;
+    std::cout << "onEntry(FSM::EventA&): " << FSM::has_onEntry<StateB, EventA>::value << std::endl;
+    std::cout << "onEntryNoArgs(): " << FSM::has_onEntryNoEventArg_v<StateB> << std::endl;
+    std::cout << "onEntry(FSM::EventA&): " << FSM::has_onEntry_v<StateB, EventA> << std::endl;
+
+    std::cout << "StateC: \n";
+    std::cout << "onEntryNoArgs(): " << FSM::has_onEntryNoEventArg<StateC>::value << std::endl;
+    std::cout << "onEntry(FSM::EventA&): " << FSM::has_onEntry<StateC, EventA>::value << std::endl;
+    std::cout << "onEntryNoArgs(): " << FSM::has_onEntryNoEventArg_v<StateC> << std::endl;
+    std::cout << "onEntry(FSM::EventA&): " << FSM::has_onEntry_v<StateC, EventA> << std::endl;
+
+    std::cout << "StateD: \n";
+    std::cout << "onEntryNoArgs(): " << FSM::has_onEntryNoEventArg<StateD>::value << std::endl;
+    std::cout << "onEntry(FSM::EventA&): " << FSM::has_onEntry<StateD, EventA>::value << std::endl;
+    std::cout << "onEntryNoArgs(): " << FSM::has_onEntryNoEventArg_v<StateD> << std::endl;
+    std::cout << "onEntry(FSM::EventA&): " << FSM::has_onEntry_v<StateD, EventA> << std::endl;
+    
+    
+    StateC{}.onEntry(FSM::Event{});
+    StateC{}.onEntry(EventA{});
     return 0;
 }
