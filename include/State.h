5f0b412421e2db5e9a5b22f021d24b7c37b07453
Maciej Adamski <madamskip1@gmail.com>
2023-11-04 22:41:02 +0100

    Move has_onEnter traits to State.h

--- include/State.h
+++ include/State.h
@@ -1,6 +1,25 @@
 #pragma once
+#include <type_traits>
 
 namespace FSM
 {
 	struct State {};
+
+	template <typename StateType, typename EventType, typename = void>
+	struct has_onEntry : std::false_type {};
+
+	template <typename StateType, typename EventType>
+	struct has_onEntry<StateType, EventType, std::void_t<decltype(std::declval<StateType>().onEntry(std::declval<EventType>()))>> : std::true_type {};
+
+	template <typename StateType, typename EventType>
+	inline constexpr bool has_onEntry_v = has_onEntry<StateType, EventType>::value;
+
+	template <typename StateType, typename = void>
+	struct has_onEntryNoEventArg : std::false_type {};
+
+	template <typename StateType>
+	struct has_onEntryNoEventArg<StateType, std::void_t<decltype(std::declval<StateType>().onEntry())>> : std::true_type {};
+
+	template <typename StateType>
+	inline constexpr bool has_onEntryNoEventArg_v = has_onEntryNoEventArg<StateType>::value;
 }
--- include/finite-state-machine.h
+++ include/finite-state-machine.h
@@ -20,25 +20,6 @@ namespace FSM
 	using variantTypeFromStatesTuple_t = typename variantTypeFromStatesTuple<StatesTuple>::type;
 
 
-	template <typename StateType, typename EventType, typename = void>
-	struct has_onEntry : std::false_type {};
-
-	template <typename StateType, typename EventType>
-	struct has_onEntry<StateType, EventType, std::void_t<decltype(std::declval<StateType>().onEntry(std::declval<EventType>()))>> : std::true_type {};
-
-	template <typename StateType, typename EventType>
-	inline constexpr bool has_onEntry_v = has_onEntry<StateType, EventType>::value;
-
-	template <typename StateType, typename = void>
-	struct has_onEntryNoEventArg : std::false_type {};
-
-	template <typename StateType>
-	struct has_onEntryNoEventArg<StateType, std::void_t<decltype(std::declval<StateType>().onEntry())>> : std::true_type {};
-
-	template <typename StateType>
-	inline constexpr bool has_onEntryNoEventArg_v = has_onEntryNoEventArg<StateType>::value;
-
-
 
 	template <typename TransitionsTable, typename InitialState = void>
 	class StateMachine
