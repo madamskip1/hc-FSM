2c62e09dc1cea19bc1a6f7ea78cc339d398e9ed5
Maciej Adamski <madamskip1@gmail.com>
2023-11-18 06:17:57 +0100

    add fsm main class tests

---
+++ tests/state_machine_tests.cpp
@@ -0,0 +1,121 @@
+#include <gtest/gtest.h>
+#include "finite-state-machine.h"
+#include "transition.h"
+#include "transitions-table.h"
+
+struct StateA {};
+struct StateB {};
+struct EventA {};
+struct EventB {};
+
+namespace FSM
+{
+    TEST(StateMachineTests, VariantFromTuple)
+    {
+        using SimpleTuple = std::tuple<int, float, double>;
+        using SimpleVariant = typename variantTypeFromStatesTuple<SimpleTuple>::type;
+        using SimpleVariant_t = variantTypeFromStatesTuple_t<SimpleTuple>;
+        auto is_same_simple = std::is_same_v<SimpleVariant, std::variant<int, float, double>>;
+        auto is_same_simple_t = std::is_same_v<SimpleVariant_t, std::variant<int, float, double>>;
+        EXPECT_EQ(is_same_simple, true);
+        EXPECT_EQ(is_same_simple_t, true);
+
+        using EmptyTuple = std::tuple<>;
+        using EmptyVariant = typename variantTypeFromStatesTuple<EmptyTuple>::type;
+        using EmptyVariant_t = variantTypeFromStatesTuple_t<EmptyTuple>;
+        auto is_same_empty = std::is_same_v<EmptyVariant, std::variant<>>;
+        auto is_same_empty_t = std::is_same_v<EmptyVariant_t, std::variant<>>;
+        EXPECT_EQ(is_same_empty, true);
+        EXPECT_EQ(is_same_empty_t, true);
+
+        using StatesTuple = std::tuple<StateA, StateB>;
+        using StatesVariant = typename variantTypeFromStatesTuple<StatesTuple>::type;
+        using StatesVariant_t = variantTypeFromStatesTuple_t<StatesTuple>;
+        auto is_same_states = std::is_same_v<StatesVariant, std::variant<StateA, StateB>>;
+        auto is_same_states_t = std::is_same_v<StatesVariant_t, std::variant<StateA, StateB>>;
+        EXPECT_EQ(is_same_states, true);
+        EXPECT_EQ(is_same_states_t, true);
+    }
+
+    TEST(StateMachineTests, InitialState)
+    {
+        using transition1 = Transition<StateA, EventA, StateB>;
+        using transitions_table = TransitionsTable<
+            transition1
+        >;
+        auto stateMachine1 = StateMachine<transitions_table>{};
+        EXPECT_EQ(stateMachine1.isInState<StateA>(), true);
+        EXPECT_EQ(stateMachine1.isInState<StateB>(), false);
+
+        auto stateMachine2 = StateMachine<transitions_table, StateB>{};
+        EXPECT_EQ(stateMachine2.isInState<StateA>(), false);
+        EXPECT_EQ(stateMachine2.isInState<StateB>(), true);
+    }
+
+    TEST(StateMachineTests, ForceTransition)
+    {
+        using transition1 = Transition<StateA, EventA, StateB>;
+        using transitions_table = TransitionsTable<
+            transition1
+        >;
+        auto stateMachine = StateMachine<transitions_table>{};
+        EXPECT_EQ(stateMachine.isInState<StateA>(), true);
+        EXPECT_EQ(stateMachine.isInState<StateB>(), false);
+
+        stateMachine.forceTransition<StateB>();
+        EXPECT_EQ(stateMachine.isInState<StateA>(), false);
+        EXPECT_EQ(stateMachine.isInState<StateB>(), true);
+
+        stateMachine.forceTransition<StateB>();
+        EXPECT_EQ(stateMachine.isInState<StateA>(), false);
+        EXPECT_EQ(stateMachine.isInState<StateB>(), true);
+    }
+
+    TEST(StateMachineTests, HandleEvent)
+    {
+        using transition1 = Transition<StateA, EventA, StateB>;
+        using transition2 = Transition<StateB, EventA, StateB>;
+        using transitions_table = TransitionsTable<
+            transition1,
+            transition2
+        >;
+
+        // handleEvent<Event>();
+
+        auto stateMachine1 = StateMachine<transitions_table>{};
+
+        auto handleEventResultTemplate1 = stateMachine1.handleEvent<EventA>();
+        EXPECT_EQ(stateMachine1.isInState<StateA>(), false);
+        EXPECT_EQ(stateMachine1.isInState<StateB>(), true);
+        EXPECT_EQ(handleEventResultTemplate1, HandleEventResult::PROCESSED);
+
+        auto handleEventResultTemplate2 = stateMachine1.handleEvent<EventA>();
+        EXPECT_EQ(stateMachine1.isInState<StateA>(), false);
+        EXPECT_EQ(stateMachine1.isInState<StateB>(), true);
+        EXPECT_EQ(handleEventResultTemplate2, HandleEventResult::PROCESSED_SAME_STATE);
+
+        auto handleEventResultTemplate3 = stateMachine1.handleEvent<EventB>();
+        EXPECT_EQ(stateMachine1.isInState<StateA>(), false);
+        EXPECT_EQ(stateMachine1.isInState<StateB>(), true);
+        EXPECT_EQ(handleEventResultTemplate3, HandleEventResult::NO_VALID_TRANSITION);
+
+        //handleEvent(Event);
+
+        auto stateMachine2 = StateMachine<transitions_table>{};
+
+        auto handleEventResult1 = stateMachine2.handleEvent(EventA{});
+        EXPECT_EQ(stateMachine1.isInState<StateA>(), false);
+        EXPECT_EQ(stateMachine1.isInState<StateB>(), true);
+        EXPECT_EQ(handleEventResult1, HandleEventResult::PROCESSED);
+
+        auto handleEventResult2 = stateMachine2.handleEvent(EventA{});
+        EXPECT_EQ(stateMachine1.isInState<StateA>(), false);
+        EXPECT_EQ(stateMachine1.isInState<StateB>(), true);
+        EXPECT_EQ(handleEventResult2, HandleEventResult::PROCESSED_SAME_STATE);
+
+        auto handleEventResult3 = stateMachine2.handleEvent(EventB{});
+        EXPECT_EQ(stateMachine1.isInState<StateA>(), false);
+        EXPECT_EQ(stateMachine1.isInState<StateB>(), true);
+        EXPECT_EQ(handleEventResult3, HandleEventResult::NO_VALID_TRANSITION);
+    }
+}
