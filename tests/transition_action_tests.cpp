7e256f7e8035e7ba3a36960837e7d90c11e32359
Maciej Adamski <madamskip1@gmail.com>
2023-11-22 12:13:30 +0100

    add test for CreateTransitionAction with struct

--- tests/transition_action_tests.cpp
+++ tests/transition_action_tests.cpp
@@ -3,22 +3,58 @@
 
 namespace FSM
 {
-    CREATE_TRANSITION_ACTION(CreateAction_test_action, source_state, event, target_state, 
+    struct StateA 
+    {
+        int value = 0;
+
+        void setValue(int v)
+        {
+            value = v;
+        }
+    };
+    struct StateB 
+    {
+        int value = 0;
+    };
+    struct EventA 
+    {
+        int value = 3;
+    };
+
+    CREATE_TRANSITION_ACTION(CreateAction_test_action, currentState, event, nextState, 
         { 
-            source_state = 1;
+            currentState = 1;
             event = 2; 
-            target_state = 3; 
+            nextState = 3; 
         }
     );
 
     TEST(TransitionActionTests, CreateAction)
     {
-        int source_state = 0;
+        int currentState = 0;
         int event = 0;
-        int target_state = 0;
-        CreateAction_test_action {} (source_state, event, target_state);
-        EXPECT_EQ(source_state, 1);
+        int nextState = 0;
+        CreateAction_test_action {} (currentState, event, nextState);
+        EXPECT_EQ(currentState, 1);
         EXPECT_EQ(event, 2);
-        EXPECT_EQ(target_state, 3);
+        EXPECT_EQ(nextState, 3);
+    }
+
+    CREATE_TRANSITION_ACTION(ActionWithStates_test_action, currentState, event, nextState,
+        {
+            currentState.setValue(1);
+            nextState.value = event.value;
+        }
+    );
+
+    TEST(TransitionActionTests, ActionWithStates)
+    {
+        StateA stateA;
+        EventA eventA { 10 };
+        StateB stateB;
+
+        ActionWithStates_test_action {} (stateA, eventA, stateB);
+        EXPECT_EQ(stateA.value, 1);
+        EXPECT_EQ(stateB.value, 10);
     }
 }
