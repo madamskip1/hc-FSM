1e9e9616d6d376ea2b70be12062089d02e2de075
Maciej Adamski <madamskip1@gmail.com>
2023-11-22 02:46:12 +0100

    Add MultiLayer AutomaticTransition tests

--- tests/automatic_transition_tests.cpp
+++ tests/automatic_transition_tests.cpp
@@ -66,4 +66,57 @@ namespace FSM
         stateMachine.handleEvent(EventA{});
         EXPECT_EQ(stateMachine.isInState<StateD>(), true);
     }
+
+        TEST(AutomaticTransitionTests, InDeeperLayerAutomaticTransition)
+    {
+        using InnerStateTransitions3 = FSM::TransitionsTable<
+            FSM::Transition<StateA, EventA, ExitState>
+        >;
+        using InnerStateMachine3 = FSM::StateMachine<InnerStateTransitions3>;
+
+        using InnerStateTransitions2 = FSM::TransitionsTable<
+            FSM::Transition<StateA, EventA, StateB>,
+            FSM::Transition<StateB, AUTOMATIC_TRANSITION, StateC>,
+            FSM::Transition<StateC, EventA, StateD>,
+            FSM::Transition<StateD, AUTOMATIC_TRANSITION, ExitState>
+            >;
+        using InnerStateMachine2 = FSM::StateMachine<InnerStateTransitions2>;
+
+         using InnerStateTransitions1 = FSM::TransitionsTable<
+            FSM::Transition<StateA, EventA, InnerStateMachine2>,
+            FSM::Transition<InnerStateMachine2, AUTOMATIC_TRANSITION, InnerStateMachine3>,
+            FSM::Transition<InnerStateMachine3, EventA, ExitState>
+            >;
+        using InnerStateMachine1 = FSM::StateMachine<InnerStateTransitions1>;
+
+        using Transitions = FSM::TransitionsTable <
+            FSM::Transition<StateA, EventA, InnerStateMachine1>,
+            FSM::Transition<InnerStateMachine1, EventA, StateB>
+        >;
+
+        auto stateMachine = FSM::StateMachine<Transitions, StateA>{};
+        EXPECT_EQ(stateMachine.isInState<StateA>(), true);
+
+        
+        stateMachine.handleEvent(EventA{});
+        auto isInInnerStateMachine1StateA = stateMachine.isInState<InnerStateMachine1, StateA>();
+        EXPECT_EQ(isInInnerStateMachine1StateA, true);
+        std::cout << "przed eventem______" << std::endl;
+        stateMachine.handleEvent(EventA{});
+        auto isInInnerStateMachine2StateA = stateMachine.isInState<InnerStateMachine1, InnerStateMachine2, StateA>();
+       EXPECT_EQ(isInInnerStateMachine2StateA, true);
+        
+        auto autoTransitResult1 = stateMachine.handleEvent(EventA{});
+        auto isInInnerStateMachine2StateC = stateMachine.isInState<InnerStateMachine1, InnerStateMachine2, StateC>();
+        EXPECT_EQ(isInInnerStateMachine2StateC, true);
+        EXPECT_EQ(autoTransitResult1, FSM::HandleEventResult::PROCESSED_INNER_STATE_MACHINE);
+
+        auto autoTransitResult2 = stateMachine.handleEvent(EventA{});
+        auto isInInnerStateMachine3StateA = stateMachine.isInState<InnerStateMachine1, InnerStateMachine3, StateA>();
+        EXPECT_EQ(isInInnerStateMachine3StateA, true);
+        EXPECT_EQ(autoTransitResult2, FSM::HandleEventResult::PROCESSED_INNER_STATE_MACHINE);
+
+        stateMachine.handleEvent(EventA{});
+        EXPECT_EQ(stateMachine.isInState<StateB>(), true);
+    }
 }
