38b8a9b139f26ec3cc467960a20a48b008886144
Maciej Adamski <madamskip1@gmail.com>
2023-11-24 09:25:35 +0100

    add method isEventResultOk

--- include/finite-state-machine.h
+++ include/finite-state-machine.h
@@ -9,6 +9,24 @@
 
 namespace FSM
 {
+	enum class HandleEventResult
+	{
+		PROCESSED,
+		PROCESSED_SAME_STATE, // Event processed, but there is no need to change state, for e.g. StateA (EventA) -> StateA
+		PROCESSED_INNER_STATE_MACHINE,
+		NO_VALID_TRANSITION,
+		EXIT_INNER_STATE_MACHINE,
+		EXIT_AUTOMATIC_INNER_STATE_MACHINE,
+		GUARD_FAILED
+	};
+
+	constexpr bool isEventResultOk(HandleEventResult result)
+	{
+		return result == HandleEventResult::PROCESSED ||
+			result == HandleEventResult::PROCESSED_SAME_STATE ||
+			result == HandleEventResult::PROCESSED_INNER_STATE_MACHINE;
+	}
+
 	template <typename Transitions_Table, typename InitialState = void>
 	class StateMachine;
 
@@ -38,18 +56,6 @@ namespace FSM
 
 	template <typename T>
 	inline constexpr bool isStateMachine_v = isStateMachine<T>::value;
-
-
-	enum class HandleEventResult
-	{
-		PROCESSED,
-		PROCESSED_SAME_STATE, // Event processed, but there is no need to change state, for e.g. StateA (EventA) -> StateA
-		PROCESSED_INNER_STATE_MACHINE,
-		NO_VALID_TRANSITION,
-		EXIT_INNER_STATE_MACHINE,
-		EXIT_AUTOMATIC_INNER_STATE_MACHINE,
-		GUARD_FAILED
-	};
 
 	template <typename Transitions_Table, typename InitialState>
 	class StateMachine
