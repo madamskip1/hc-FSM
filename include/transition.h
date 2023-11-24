8c0719f46c094ab6d6b5078688ce9fe187741cc9
Maciej Adamski <madamskip1@gmail.com>
2023-11-24 03:46:05 +0100

    fix isValidTransition after adding Guard

--- include/transition.h
+++ include/transition.h
@@ -22,8 +22,8 @@ namespace FSM
 	template <typename Transition>
 	struct isValidTransition : std::false_type {};
 
-	template <typename BeforeStateType, typename EventTriggerType, typename NextStateType, typename Action>
-	struct isValidTransition<Transition<BeforeStateType, EventTriggerType, NextStateType, Action>> : std::true_type {};
+	template <typename BeforeStateType, typename EventTriggerType, typename NextStateType, typename Action, typename Guard>
+	struct isValidTransition<Transition<BeforeStateType, EventTriggerType, NextStateType, Action, Guard>> : std::true_type {};
 
 	template <typename Transition>
 	static constexpr bool isValidTransition_v = isValidTransition<Transition>::value;
