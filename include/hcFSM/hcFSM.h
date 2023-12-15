357d8b0c1a2538961ed948cf6a9b2a4dbacf33dc
Maciej Adamski <madamskip1@gmail.com>
2023-12-16 00:40:22 +0100

    All single include header hcFSM.h

---
+++ include/hcFSM/hcFSM.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "hcFSM/handle-event-result.h"
+#include "hcFSM/state.h"
+#include "hcFSM/transition.h"
+#include "hcFSM/transition_action.h"
+#include "hcFSM/transition-guard.h"
+#include "hcFSM/transitions-table.h"
+#include "hcFSM/fsm-traits.h"
+#include "hcFSM/finite-state-machine.h"
+
