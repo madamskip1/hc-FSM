692b88de9f2add72606512162ad0971c75ca71a9
Maciej Adamski <madamskip1@gmail.com>
2024-02-12 20:10:52 +0100

    Add HostileNPC example

---
+++ examples/Hostile_NPC/events.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct Died {};
+struct PlayerSpotted {};
+struct PlayerKilled {};
+struct PlayerFled {};
+struct LowHealth {};
+struct HighHealth {};
+struct PlayerWithinRange {};
+struct PlayerOutOfRange {};
