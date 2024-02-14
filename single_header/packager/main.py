014616cad5d572f6b8be23026593434414f36767
Maciej Adamski <madamskip1@gmail.com>
2024-02-14 21:46:36 +0100

    autopep8 format python packager files

--- single_header/packager/main.py
+++ single_header/packager/main.py
@@ -4,24 +4,28 @@ from packager import Packager
 
 here_path = os.path.abspath(os.path.dirname(__file__))
 
+
 def __save_single_header(single_header):
     with open("single_header.h", "w") as file:
         file.writelines(single_header)
 
+
 def __process_file(packager, file_path):
     with open(file_path, "r") as file:
         packager.process_file(file)
+
 
 def __create_absolute_path_to_header(file_path):
     global here_path
     to_detail_path = "../../include/hcFSM/detail/"
     return os.path.join(here_path, to_detail_path, file_path)
 
+
 def __get_project_version():
     global here_path
     cmake_path = os.path.join(here_path, "../../CMakeLists.txt")
     version_regex_pattern = r'(?<=VERSION\s)\d+(?:\.\d+)*'
-    
+
     with open(cmake_path, "r") as file:
         content = file.read()
         match = re.findall(version_regex_pattern, content)
@@ -29,10 +33,9 @@ def __get_project_version():
             return match[1]
         else:
             return None
-    
+
+
 def main():
-    project_version = __get_project_version()
-    packager = Packager(project_version)
     files_to_process = (
         "state-machine-fwd.h",
         "handle-event-result.h",
@@ -45,10 +48,13 @@ def main():
         "state-machine.h"
     )
 
+    project_version = __get_project_version()
+    packager = Packager(project_version)
+
     for file in files_to_process:
         file_path = __create_absolute_path_to_header(file)
         __process_file(packager, file_path)
-    
+
     single_header = packager.pack_headers()
     __save_single_header(single_header)
 
--- single_header/packager/packager.py
+++ single_header/packager/packager.py
@@ -3,7 +3,7 @@ from datetime import date
 
 
 class Packager:
-    def __init__(self, project_version = None):
+    def __init__(self, project_version=None):
         self.include_files = set()
         self.files_content = []
         self.project_version = project_version
@@ -13,9 +13,10 @@ class Packager:
         file_content = self.__remove_pragma_once(file_content)
         self.__find_includes(file_content)
         file_content = self.__remove_includes(file_content)
-        file_content = self.__strip_blank_lines_at_the_end_of_file(file_content)
+        file_content = self.__strip_blank_lines_at_the_end_of_file(
+            file_content)
         self.files_content.append(file_content)
-            
+
     def pack_headers(self):
         single_header_content = []
         single_header_content.append(self.__prepare_metadata_header())
@@ -26,7 +27,7 @@ class Packager:
         for file_content in self.files_content:
             single_header_content.append('\n')
             single_header_content.extend(file_content)
-        
+
         return single_header_content
 
     def __find_includes(self, file_content):
@@ -35,23 +36,24 @@ class Packager:
             line = line.strip()
             if not line or line == "":
                 continue
-            
+
             match = include_patern.match(line)
             if match:
                 self.include_files.add(match.group(1) + '\n')
             else:
-                break  
-    
+                break
+
     def __remove_pragma_once(self, file_content):
-        file_content = file_content[1:]  # Remove first line, because its always #pragma once
+        # Remove first line, because its always #pragma once
+        file_content = file_content[1:]
         return file_content
 
     def __remove_includes(self, file_content):
         for (i, line) in enumerate(file_content):
-            if not(not line or line.startswith("#include") or line == "" or line == '\n'):
+            if not (not line or line.startswith("#include") or line == "" or line == '\n'):
                 break
         return file_content[i:]
-    
+
     def __strip_blank_lines_at_the_end_of_file(self, file_content):
         last_non_blank_line = len(file_content)
         for i in reversed(range(len(file_content))):
@@ -59,9 +61,9 @@ class Packager:
             if line and line != "":
                 break
             last_non_blank_line -= 1
-            
+
         return file_content[:last_non_blank_line]
-    
+
     def __prepare_metadata_header(self):
         metadata_header = "/**\n"
         metadata_header += " * @author: Adamski Maciej (madamskip1)\n"
@@ -72,5 +74,5 @@ class Packager:
         if self.project_version is not None:
             metadata_header += " * @version: " + self.project_version + "\n"
         metadata_header += " */"
-        
+
         return metadata_header
