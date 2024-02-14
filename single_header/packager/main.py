2527b31f8f58949fc3181c65fb88166f28c1d9d9
Maciej Adamski <madamskip1@gmail.com>
2024-02-14 20:16:27 +0100

    Process all files in packager

--- single_header/packager/main.py
+++ single_header/packager/main.py
@@ -1,20 +1,39 @@
 import os
 from packager import Packager
 
-def save_single_header(single_header):
+def __save_single_header(single_header):
     with open("single_header.h", "w") as file:
         file.writelines(single_header)
 
+def __process_file(packager, file_path):
+    with open(file_path, "r") as file:
+        packager.process_file(file)
+
+def __create_absolute_path(file_path):
+    here_path = os.path.abspath(os.path.dirname(__file__))
+    to_detail_path = "../../include/hcFSM/detail/"
+    return os.path.join(here_path, to_detail_path, file_path)
+    
 def main():
     packager = Packager()
-    path_h = "../../include/hcFSM/detail/fsm-traits.h"
-    my_path = os.path.abspath(os.path.dirname(__file__))
-    path = os.path.join(my_path, path_h)
-    with open(path, "r") as file:
-        packager.process_file(file)
+    files_to_process = (
+        "state-machine-fwd.h",
+        "handle-event-result.h",
+        "state.h",
+        "transition.h",
+        "transition-action.h",
+        "transition-guard.h",
+        "transitions-table.h",
+        "fsm-traits.h",
+        "state-machine.h"
+    )
+
+    for file in files_to_process:
+        file_path = __create_absolute_path(file)
+        __process_file(packager, file_path)
     
     single_header = packager.pack_headers()
-    save_single_header(single_header)
+    __save_single_header(single_header)
 
 if __name__ == "__main__":
     main()
