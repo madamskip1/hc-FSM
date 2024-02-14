368218c53a6bbd2228ec901415f9ba9c7bc1fd00
Maciej Adamski <madamskip1@gmail.com>
2024-02-14 19:45:06 +0100

    Remove include directives from headers content

--- single_header/packager/packager.py
+++ single_header/packager/packager.py
@@ -4,6 +4,7 @@ import re
 class Packager:
     def __init__(self):
         self.include_files = set()
+        self.files_content = []
         
 
     def parse_file(self, file_name):
@@ -11,6 +12,8 @@ class Packager:
             file_content = file.readlines()
             file_content = self.remove_pragma_once(file_content)
             self.find_includes(file_content)
+            file_content = self.remove_includes(file_content)
+            self.files_content.append(file_content)
 
     def find_includes(self, file_content):
         include_patern = re.compile(r'(#include\s*<.*>)')
@@ -28,3 +31,9 @@ class Packager:
     def remove_pragma_once(self, file_content):
         file_content = file_content[1:]  # Remove first line, because its always #pragma once
         return file_content
+
+    def remove_includes(self, file_content):
+        for (i, line) in enumerate(file_content):
+            if not(not line or line.startswith("#include") or line == ""):
+                break
+        return file_content[i:]
